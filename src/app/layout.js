7bb7c8235280e83b33156440ba9ae24daa80daa2
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2025-08-18 17:22:36 +0530

    upd --j

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import Footer from "@/components/footer/Footer";
 import "./globals.scss";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Ratnashri Aluminium",
@@ -19,8 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {  
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body 
+      // className={`${geistSans.variable} ${geistMono.variable}`}
+      >
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/firstSection/page.jsx
@@ -0,0 +1,17 @@
+import "./firstSection.scss";
+const FirstSection = () => {
+
+  return (
+    <div className="FirstSectionWarpper">
+        <div className="InnerSectionWrapper">
+            <p className="InnerWapper">Shaping the Future with Precision</p>
+        
+            <p className="InnerWapper">Aluminium Extrusions</p>
+        
+        </div>
+        
+        
+    </div>
+  );
+};
+export default FirstSection;
