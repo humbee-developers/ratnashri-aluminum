c273692a797618a94c0b78760b1af95b118cc03d
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2025-09-09 10:20:22 +0530

    upd -j

--- src/components/firstSection/page.jsx
+++ src/components/firstSection/page.jsx
@@ -1,6 +1,17 @@
 import "./firstSection.scss";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
 const FirstSection = () => {
+  const [startAnimation, setStartAnimation] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setStartAnimation(true);
+    }, 11000); // 8 seconds delay
+
+    return () => clearTimeout(timer); // cleanup
+  }, []);
 
   return (
     <div className="FirstSectionWarpper">
@@ -10,35 +21,47 @@ const FirstSection = () => {
             opacity: 0,
             y: 80,
           }}
-          whileInView={{
-            opacity: 1,
-            y: 0,
-            transition: {
-              duration: 1.5,
-            },
-          }}
-          viewport={{ once: true }}
-          className="InnerWapper">Shaping the Future with Precision</motion.p>
+          animate={
+            startAnimation
+              ? {
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    duration: 1.5,
+                  },
+                }
+              : {}
+          }
+          className="InnerWapper"
+        >
+          Shaping the Future with Precision
+        </motion.p>
 
         <motion.p
           initial={{
             opacity: 0,
             y: 80,
           }}
-          whileInView={{
-            opacity: 1,
-            y: 0,
-            transition: {
-              duration: 1.5,
-            },
-          }}
-          viewport={{ once: true }}
-          className="InnerWapper" id="Wrap">Aluminium Extrusions</motion.p>
-
+          animate={
+            startAnimation
+              ? {
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    duration: 1.5,
+                    delay: 0.3, // delay between two lines if needed
+                  },
+                }
+              : {}
+          }
+          className="InnerWapper"
+          id="Wrap"
+        >
+          Aluminium Extrusions
+        </motion.p>
       </div>
-
-
     </div>
   );
 };
+
 export default FirstSection;
